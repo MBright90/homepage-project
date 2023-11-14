9952f5f86bfc43bc03a1841178c2c4a983ee77b1
mbright90 <michael.bright90@icloud.com>
2023-11-14 11:02:01 +0000

    Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -39,12 +39,7 @@ module.exports = {
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@customTypes': path.resolve(__dirname, 'src/types'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@style': path.resolve(__dirname, 'src/style'),
     },
   },
 };
