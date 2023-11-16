c9a70fa27c38f30538cbc30ab9ab2f801004f3da
mbright90 <michael.bright90@icloud.com>
2023-11-16 09:48:43 +0000

    Create image export file

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import headerSml from './header-portrait-500.jpg'
+import headerMed from './header-portrait-640.jpg'
+import headerLrg from './header-portrait-1280.jpg'
+
+const headerImages = { headerSml, headerMed, headerLrg }
+
+export { headerImages }
