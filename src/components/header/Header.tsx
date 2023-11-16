c721a7a60b0031b74140bccc3c5e457e82c714ab
mbright90 <michael.bright90@icloud.com>
2023-11-16 09:57:46 +0000

    Structure img tag

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,21 @@
 import type React from 'react'
+import { headerImages } from '@assets/images'
 
 import style from './Header.module.scss'
 
+const { headerSml, headerMed, headerLrg } = headerImages
+
 const Header: React.FC = () => {
-  return <header className={style.headerContainer}></header>
+  return (
+    <header className={style.headerContainer}>
+      <img
+        className={style.headerImage}
+        src={headerSml}
+        srcSet={`${headerSml} 500w, ${headerMed} 640w, ${headerLrg} 1000w`}
+        alt="Image of a businessman"
+      />
+    </header>
+  )
 }
 
 export default Header
