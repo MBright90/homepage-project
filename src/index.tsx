5995bd240459ff0144f558e6fea872086e616c22
mbright90 <michael.bright90@icloud.com>
2023-11-17 10:28:21 +0000

    Add font import and structure app entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import { createRoot } from 'react-dom/client'
 
+import App from './app'
+
 import '@style/style.scss'
 import '@style/style-reset.scss'
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app')!)
-root.render(<h1>Hello, world</h1>)
+root.render(<App />)
