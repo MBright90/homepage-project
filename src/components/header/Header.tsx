d702b2f6f6361821e064b05cd5cafa922420401f
mbright90 <michael.bright90@icloud.com>
2023-11-19 10:28:38 +0000

    Add about me structure and text

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -18,7 +18,18 @@ const Header: React.FC = () => {
         <div className={style.backgroundSlant}></div>
       </div>
       <div className={style.name}>MICHAEL</div>
-      <div className={style.aboutMeContainer}></div>
+      <div className={style.aboutMeContainer}>
+        <div className={style.textPosition}></div>
+        <h1 className={style.aboutMeTitle}>ABOUT ME</h1>
+        <p className={style.aboutMePara}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum
+        </p>
+      </div>
     </header>
   )
 }
