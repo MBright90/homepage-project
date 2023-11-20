7e4747881658e4b88fc8f0fb3c831a805dc6e983
mbright90 <michael.bright90@icloud.com>
2023-11-20 17:15:16 +0000

    Initiate projectsGrid directory and export

---
+++ src/components/projectsGrid/ProjectsGrid.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+import style from './ProjectsGrid.module.scss'
+
+const ProjectsGrid: React.FC = () => {
+  return <div className={style.gridContainer}></div>
+}
+
+export default ProjectsGrid
