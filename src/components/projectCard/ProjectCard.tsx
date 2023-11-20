1da15c3229ab58a90870c5e5be5bac6e5904347f
mbright90 <michael.bright90@icloud.com>
2023-11-20 17:22:23 +0000

    Initiate projectCard directory and export

---
+++ src/components/projectCard/ProjectCard.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+import style from './ProjectCard.module.scss'
+
+const ProjectCard: React.FC = () => {
+  return <div className={style.cardContainer}></div>
+}
+
+export default ProjectCard
