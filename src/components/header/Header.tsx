ce3519d1cb70a5f9ee1c84ea5820da5792fb8ff5
mbright90 <michael.bright90@icloud.com>
2023-11-20 17:14:48 +0000

    Remove backgroundSlant div

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,15 +8,12 @@ const { headerSml, headerMed, headerLrg } = headerImages
 const Header: React.FC = () => {
   return (
     <header className={style.headerContainer}>
-      <div className={style.imageContainer}>
-        <img
-          className={style.headerImage}
-          src={headerSml}
-          srcSet={`${headerSml} 500w, ${headerMed} 640w, ${headerLrg} 1000w`}
-          alt="Image of a businessman"
-        />
-        <div className={style.backgroundSlant}></div>
-      </div>
+      <img
+        className={style.headerImage}
+        src={headerSml}
+        srcSet={`${headerSml} 500w, ${headerMed} 640w, ${headerLrg} 1000w`}
+        alt="Image of a businessman"
+      />
       <div className={style.name}>MICHAEL</div>
       <div className={style.aboutMeContainer}>
         <div className={style.textPosition}></div>
