4a2857c5fc67e0fc194aa3ac328be5b3237b9b82
mbright90 <michael.bright90@icloud.com>
2023-11-21 12:13:04 +0000

    Restructure component and remove unnecessary divs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,24 +8,31 @@ const { headerSml, headerMed, headerLrg } = headerImages
 const Header: React.FC = () => {
   return (
     <header className={style.headerContainer}>
-      <img
+      {/* <img
         className={style.headerImage}
         src={headerSml}
         srcSet={`${headerSml} 500w, ${headerMed} 640w, ${headerLrg} 1000w`}
         alt="Image of a businessman"
-      />
+      /> */}
       <div className={style.name}>MICHAEL</div>
       <div className={style.aboutMeContainer}>
-        <div className={style.textPosition}></div>
-        <h1 className={style.aboutMeTitle}>ABOUT ME</h1>
-        <p className={style.aboutMePara}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum
-        </p>
+        <img
+          className={style.headerImage}
+          src={headerSml}
+          srcSet={`${headerSml} 500w, ${headerMed} 640w, ${headerLrg} 1000w`}
+          alt="Image of a businessman"
+        />
+        <div className={style.textContainer}>
+          <h1 className={style.aboutMeTitle}>ABOUT ME</h1>
+          <p className={style.aboutMePara}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum
+          </p>
+        </div>
       </div>
     </header>
   )
