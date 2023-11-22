e1a4f5c5fe5b3d80344d1b41bcc02a5e0bb9f298
mbright90 <michael.bright90@icloud.com>
2023-11-22 10:25:11 +0000

    Add image positioning and card sizing

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -3,7 +3,14 @@ import type React from 'react'
 import style from './ProjectCard.module.scss'
 
 const ProjectCard: React.FC = () => {
-  return <div className={style.cardContainer}></div>
+  return (
+    <div className={style.cardContainer}>
+      <div className={style.cardImageContainer}>
+        {/* <img src={'null'} className={style.cardImage} alt="hello" /> */}
+        <div className={style.ProjectTitle}>projectTitle</div>
+      </div>
+    </div>
+  )
 }
 
 export default ProjectCard
