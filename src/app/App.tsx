b87652be71da1da7c79901abd2c27fd70a16e1ea
mbright90 <michael.bright90@icloud.com>
2023-11-22 10:10:37 +0000

    Add ProjectsGrid to app flow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,14 @@
 import type React from 'react'
 import Header from '@components/header/Header'
+import ProjectsGrid from '@components/projectsGrid/ProjectsGrid'
 
 const App: React.FC = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <ProjectsGrid />
+    </>
+  )
 }
 
 export default App
