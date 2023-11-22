71f3356c2112763fcd6064e334336b44c83f717d
mbright90 <michael.bright90@icloud.com>
2023-11-22 10:25:34 +0000

    Add card elements and grid positioning

--- src/components/projectsGrid/ProjectsGrid.tsx
+++ src/components/projectsGrid/ProjectsGrid.tsx
@@ -1,9 +1,19 @@
 import type React from 'react'
+import ProjectCard from '@components/projectCard/ProjectCard'
 
 import style from './ProjectsGrid.module.scss'
 
 const ProjectsGrid: React.FC = () => {
-  return <div className={style.gridContainer}></div>
+  return (
+    <div className={style.gridContainer}>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
+  )
 }
 
 export default ProjectsGrid
