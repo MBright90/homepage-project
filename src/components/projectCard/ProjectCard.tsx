88ef375453d1fdffeecb9a53b47881fa01d2639f
mbright90 <michael.bright90@icloud.com>
2023-11-23 10:17:31 +0000

    Add para structure elements

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -9,6 +9,13 @@ const ProjectCard: React.FC = () => {
         {/* <img src={'null'} className={style.cardImage} alt="hello" /> */}
         <div className={style.ProjectTitle}>projectTitle</div>
       </div>
+      <div className={style.cardTextContainer}>
+        <p className={style.cardText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
   )
 }
