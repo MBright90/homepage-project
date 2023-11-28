b85e8259fd0e8a7a1f0116c79bf4a9ded3798f32
mbright90 <michael.bright90@icloud.com>
2023-11-28 13:42:51 +0000

    Add structure to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,28 @@ import type React from 'react'
 import style from './Footer.module.scss'
 
 const Footer: React.FC = () => {
-  return <footer className={style.footer}></footer>
+  return (
+    <footer className={style.footer}>
+      <div className={style.textContainer}>
+        <h1 className={style.footerTitle}>Contact Me!</h1>
+        <p className={style.footerPara}> </p>
+        <div className={style.contactDetailsContainer}>
+          <p className={style.contactDetail}>
+            <span className={style.contactIcon}></span> {/* email */}
+          </p>
+          <p className={style.contactDetail}>
+            <span className={style.contactIcon}></span> {/* phone */}
+          </p>
+        </div>
+        <div className={style.iconContainer}>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" />
+        </div>
+      </div>
+      <div className={style.imageContainer}></div>
+    </footer>
+  )
 }
 
 export default Footer
