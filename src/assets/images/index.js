4947b3a73b24d001f0c6c49926c2af7044ad58e4
mbright90 <michael.bright90@icloud.com>
2023-11-29 16:32:54 +0000

    Add footer portrait to image exports

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,8 @@
+import footerSml from './footer-portrait640.jpg'
 import headerSml from './header-portrait-500.jpg'
 import headerMed from './header-portrait-640.jpg'
 import headerLrg from './header-portrait-1280.jpg'
 
 const headerImages = { headerSml, headerMed, headerLrg }
 
-export { headerImages }
+export { headerImages, footerSml }
