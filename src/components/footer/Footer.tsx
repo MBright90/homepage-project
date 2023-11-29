9d7664fcdfc76074e740c903f9e0798848506f4d
mbright90 <michael.bright90@icloud.com>
2023-11-29 16:32:32 +0000

    Add elements to footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,7 @@
 import type React from 'react'
+import { footerSml } from '@assets/images'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import style from './Footer.module.scss'
 
@@ -7,22 +10,38 @@ const Footer: React.FC = () => {
     <footer className={style.footer}>
       <div className={style.textContainer}>
         <h1 className={style.footerTitle}>Contact Me!</h1>
-        <p className={style.footerPara}> </p>
+        <p className={style.footerPara}>
+          Please get in touch if you think our work could be mutually beneficial
+        </p>
         <div className={style.contactDetailsContainer}>
           <p className={style.contactDetail}>
-            <span className={style.contactIcon}></span> {/* email */}
+            <span className={style.contactIcon}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            this-is-an-email@example.com
           </p>
           <p className={style.contactDetail}>
-            <span className={style.contactIcon}></span> {/* phone */}
+            <span className={style.contactIcon}>
+              <FontAwesomeIcon icon={faPhone} />
+            </span>
+            07777 999777
           </p>
         </div>
         <div className={style.iconContainer}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" />
+          <a href="www.twitter.com">
+            <i className={`devicon-twitter-original ${style.iconLink}`}></i>
+          </a>
+          <a href="www.linkedIn.com">
+            <i className={`devicon-linkedin-plain ${style.iconLink}`}></i>
+          </a>
+          <a href="www.github.com">
+            <i className={`devicon-github-original ${style.iconLink}`}></i>
+          </a>
         </div>
       </div>
-      <div className={style.imageContainer}></div>
+      <div className={style.imageContainer}>
+        <img src={footerSml} alt="image of a business man outside" />
+      </div>
     </footer>
   )
 }
